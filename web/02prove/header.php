004377d1ba97f0465bd1790351e8b625c9723505
ericburdett <eric@burdett.us>
2018-01-20 15:26:32 -0700

    Moved 02prove to web directory.

---
+++ web/02prove/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+  <head>
+    <meta charset="utf-8">
+    <title>My Header</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+
+  <body>
+  <h1>Eric Burdett</h1>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="home.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="assignments.php">Assignments</a>
+      </li>
+    </ul>
+  </nav> 
+  
+  </body>
+</html>
