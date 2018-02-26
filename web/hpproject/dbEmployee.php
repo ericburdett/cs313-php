91cd3f67f1bf7a808f9e8c8d8855c08fd3afb333
ericburdett <eric@burdett.us>
2018-02-26 07:25:32 -0700

    Fixed update header for Employees

--- web/hpproject/dbEmployee.php
+++ web/hpproject/dbEmployee.php
@@ -174,7 +174,7 @@ foreach($stmt->fetchAll() as $row)
       <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title">Update Solution</h5>
+          <h5 class="modal-title">Update Employee</h5>
           <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
